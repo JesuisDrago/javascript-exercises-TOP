f44a5f5c258f83f82c410d2c001ab4ac2eab67ba
JesuisDrago <iliedragosgabriel@gmail.com>
2024-10-28 18:27:05 +0200

    Made a simple celsius-fahrenheit grades conversion calculator

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrGrades) {
+  return Math.round((fahrGrades - 32)*(5 / 9) * 10) / 10 ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celGrades) {
+  return Math.round(((celGrades * (9 / 5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
