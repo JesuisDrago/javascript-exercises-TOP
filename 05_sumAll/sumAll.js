ee5550e8dc15ceff08e460d37aaa219875336215
JesuisDrago <iliedragosgabriel@gmail.com>
2024-10-28 17:52:53 +0200

    Did the JS file to sum up all the positive integers between 2 given numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstElement, secondElement) {
 
+    if(firstElement < 0 || secondElement < 0) return "ERROR";
+    if(Number.isInteger(firstElement) === false || Number.isInteger(secondElement) === false) return "ERROR";
+        
+
+
+let sum = 0;
+
+if(firstElement <= secondElement){
+    for ( let i = firstElement; i <= secondElement; i++){
+        sum += i; 
+    }
+} else {
+    for ( let i = secondElement; i <= firstElement; i++){
+        sum += i; 
+    }
+ }
+return sum;
 };
 
 // Do not edit below this line
